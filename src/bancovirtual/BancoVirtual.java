2aa4887e7e9556a62d4e6fa0cc95968f907743d6
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2023-01-31 11:21:33 -0300

    Add files via upload

---
+++ src/bancovirtual/BancoVirtual.java
@@ -0,0 +1,11 @@
+package bancovirtual;
+
+public class BancoVirtual {
+
+	public static void main(String[] args) {
+
+		Conta conta1 = new Conta(4550, 13040);
+				
+	}
+
+}
---
+++ src/bancovirtual/Conta.java
@@ -0,0 +1,67 @@
+package bancovirtual;
+
+public class Conta {
+
+	private double saldo;
+	private int agencia;
+	private int numeroConta;
+	private Cliente titular;
+	
+	public Conta(int agencia, int numeroConta) {
+		this.agencia = agencia;
+		this.numeroConta = numeroConta;
+		System.out.println("Uma conta está sendo criada, com agência = " +agencia+ " e conta = " +numeroConta+ ".");
+	}
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+}
